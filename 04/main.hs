860ac466a761b432714ed58c4e4346caa984e58e
Caleb Larsen <kneeawn@gmail.com>
2023-12-04 12:31:04 -0600

    Part 2

--- 04/main.hs
+++ 04/main.hs
@@ -1,20 +1,36 @@
 import Text.ParserCombinators.Parsec
 import qualified Data.Set as Set
-
+import Data.Array
 
 type Card = (Set.Set Int, [Int])
 
 solve :: String -> Int
 solve s =
-  case val of
-    (Left p) -> 0
-    (Right v) -> v
-  where val = sum . map (`lineVal` 0) <$> cards
+  case finalCards of
+    (Left _) -> 0
+    (Right (Left _)) -> 0
+    (Right (Right cs)) -> sum [cs!i | i <- [1..length cs]]
+  where finalCards = (\x -> (\y -> addCardsN (length x) 1 y x) <$> vals) <$> arr
+        arr   = (\x -> array (1,length x) [(i, 1) | i <- [1..length x]]) <$> vals
+        vals  = map (`lineVal` 0) <$> cards
         cards = parse pCards "" s
+
+addCard :: Int -> Int ->  Array Int Int -> Array Int Int
+addCard _ 0 currCards = currCards
+addCard i offset currCards = newCards // [(index, (newCards!index)+(newCards!i))]
+  where newCards = addCard i (offset-1) currCards
+        index = i+offset
+
+addCardsN :: Int -> Int -> [Int] -> Array Int Int -> Array Int Int
+addCardsN most i vals currCards
+  | i > most = currCards
+  | otherwise = addCardsN most (i+1) vals newCards
+    where newCards = addCard i (vals!!(i-1)) currCards
+
 
 lineVal :: Card -> Int -> Int
 lineVal (x, y:ys) running
-  | Set.member y x = if running == 0 then lineVal (x, ys) 1 else lineVal (x, ys) 2*running
+  | Set.member y x = if running == 0 then lineVal (x, ys) 1 else lineVal (x, ys) running+1
   | otherwise = lineVal (x, ys) running
 lineVal (_, []) running = running
 
